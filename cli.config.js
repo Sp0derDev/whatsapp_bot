efe15480abca02681fbe0562700645935c932d42
M SHAH <shah@idk.uno>
2023-05-02 09:12:02 +0100

    🔧 cleaner config

--- cli.config.js
+++ cli.config.js
@@ -1,20 +1,19 @@
 exports.default = () => {
 
     return {
-       "webhook": "https://webhook.site/858d8e20-2fcb-4d18-b2b7-c700d3e00d70",
-        "ev": "https://webhook.site/858d8e20-2fcb-4d18-b2b7-c700d3e00d70",
+        // "webhook": "https://webhook.site/13d8deee-0bd8-48a9-b8c5-1e17f699b389",
+        // "ev": "https://webhook.site/858d8e20-2fcb-4d18-b2b7-c700d3e00d70",
         "ef": "*", 
         "socket": true,
         "disableSpins": true, 
         "port": 8899, 
-        "autoRefresh": true,
         "sessionId": "Ahmed",
         "killProcessOnBrowserClose": true,
+        "skipSavePostmanCollection": true,
         "logging": [{ "type": "console" }],
         "debug": true,
         "logConsoleErrors": true,
         "licenseKey": "6106AABF-7E1E4631-BB6F5D01-F4ED8C6F",
-        "callTimeout": 300000,
         // "tunnel": true,
 		//"useChrome": true,
 		"headful": true,
